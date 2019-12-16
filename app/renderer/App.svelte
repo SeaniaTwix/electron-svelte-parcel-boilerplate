dbaad6c2afee4ea0e5074fa738798bd6a4a59572
Seania Twix <seania.twix@gmail.com>
2019-12-17 05:16:54 +0900

    .

--- app/renderer/App.svelte
+++ app/renderer/App.svelte
@@ -1,7 +1,15 @@
 <script>
+  import { onMount } from 'svelte'
   import ElectronService from './lib/electron-service';
 
   const es = new ElectronService()
+
+  onMount(() => {
+    console.log(req, nav)
+  })
+
+  export let req
+  export let nav
 </script>
 
-<p>hello world</p>
+<p>Current Path is '{req.pathname}'</p>
